1da0850c871524e0bee97a6001d62cac16287213
maltseva-k <k.obydennova@gmail.com>
2022-04-06 16:01:16 +0300

    correcting the code after applying eslint, removing unnecessary output to the console

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,57 @@
-const path = require('path');
-const CopyWebpackPlugin= require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ESLintPlugin = require('eslint-webpack-plugin');
-
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
 
 module.exports = {
-    devServer: {
-      hot: true
-    },
-    entry: [
-        './src/js/index.js',
-        './src/style/main.scss'
-    ],
-    output: {
-        filename: './js/bundle.js'
-    },
-    devtool: "source-map",
-    module: {
-        rules: [
-                {
-                    test: /\.(sass|scss)$/,
-                    include: path.resolve(__dirname, 'src/style'),
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        "css-loader",
-                        "sass-loader",
-                    ],
-                },
+  devServer: {
+    hot: true
+  },
+  entry: [
+    './src/js/index.js',
+    './src/style/main.scss'
+  ],
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/style'),
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new ESLintPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "./css/[name].css"
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: './src/fonts',
-                    to: './fonts'
-                },
-                {
-                    from: './src/favicon',
-                    to: './favicon'
-                },
-                {
-                    from: './src/img',
-                    to: './img'
-                },
-                {
-                    from: './src/index.html',
-                    to: './index.html'
-                }
-            ]
-        }),
+      }
     ]
-};
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new MiniCssExtractPlugin({
+      filename: './css/[name].css'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './src/fonts',
+          to: './fonts'
+        },
+        {
+          from: './src/favicon',
+          to: './favicon'
+        },
+        {
+          from: './src/img',
+          to: './img'
+        },
+        {
+          from: './src/index.html',
+          to: './index.html'
+        }
+      ]
+    })
+  ]
+}
