804caa732a5e972743400001d7a8b6835c4d4a3c
maltseva-k <k.obydennova@gmail.com>
2022-04-06 13:57:24 +0300

    initial commit: server start

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+    devServer: {
+      hot: true
+    },
+    entry: [
+        './src/js/index.js',
+        './src/style/main.scss'
+    ],
+    output: {
+        filename: './js/bundle.js'
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+                {
+                    test: /\.(sass|scss)$/,
+                    include: path.resolve(__dirname, 'src/style'),
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "sass-loader",
+                    ],
+                },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./css/[name].css"
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './src/fonts',
+                    to: './fonts'
+                },
+                {
+                    from: './src/favicon',
+                    to: './favicon'
+                },
+                {
+                    from: './src/img',
+                    to: './img'
+                },
+                {
+                    from: './src/index.html',
+                    to: './index.html'
+                }
+            ]
+        }),
+    ]
+};
