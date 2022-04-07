817f8991d31c1f10a166977c311c1f4cf0083317
maltseva-k <k.obydennova@gmail.com>
2022-04-07 10:13:58 +0300

    add block card (with js and start-css-style) & replace <script>

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const button = document.querySelector('.button')
-
+const cards = document.querySelectorAll('.card')
 button.addEventListener('mouseover', function () {
   this.classList.add('button_active')
 })
@@ -7,3 +7,16 @@ button.addEventListener('mouseover', function () {
 button.addEventListener('mouseout', function () {
   this.classList.remove('button_active')
 })
+
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('mouseover', function () {
+    const activeCard = document.querySelectorAll('.card_active')
+    if (activeCard.length) {
+      activeCard[0].className = activeCard[0].className.replace('card_active', '')
+    }
+    this.classList.add('card_active')
+  })
+  cards[i].addEventListener('mouseout', function () {
+    this.classList.remove('card_active')
+  })
+}
